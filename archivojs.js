548fec03e83f7533271a8d4bbea10383a89dc34a
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 00:06:32 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,17 +1,4 @@
-let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
-.then(response => response.json)
-.then(data => {
-   function appendData(data) {
-  var m1 = document.getElementById("myData");
-  for (let i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
- m1.appendChild(div);
-  }
-}})
-      
+fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+.then(response => response.json())
+.then(data => console.log(data))
 .catch(error => console.log(error))
-
-
-console.log(promise) 
-console.log(typeof promise)
