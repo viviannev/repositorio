072ffba775938faa754a79fb6438d22fa442ca81
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 23:26:01 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,4 +1,4 @@
-   fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
  .then(function (response) {
     return response.json();
   })
