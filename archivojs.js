fa1e1915ea73ea92b527315fbeb821e25a5c5aba
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 21:43:22 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -9,7 +9,4 @@ let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101
 console.log(promise) 
 console.log(typeof promise)
 
-promise.forEach(let i = 0; i < data.length; i++) { 
-document.getElementById("myData").innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
-            }
-
+promise.forEach(products => console.log(products));
