b8a9037d23f77505c6603d21d4208b732dc0ffc4
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 23:51:54 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -2,13 +2,15 @@ const promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/1
  .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-function appendData(data) {
-  let m1 = document.getElementById("myData");
- promise.forEach (let i = 0; i < data.length; i++) {
-    document.getElementById("myData").innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
-
-  }}});
+  .then(data => {
+   function appendData(data) {
+  var m1 = document.getElementById("myData");
+  for (let i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
+ m1.appendChild(div);
+  }
+}
   
 
 
