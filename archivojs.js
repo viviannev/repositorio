fe666de9a445223246df9eb85f605c3c6ab046c6
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 23:14:48 -0300

    Update and rename archivojs.html to archivojs.js

---
+++ archivojs.js
@@ -0,0 +1,11 @@
+ fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+.then(response => response.json)
+ .then(data => {
+     console.log(data)
+ })
+.catch(error => console.log(error))
+
+
+
+console.log(promise) 
+console.log(typeof promise)
