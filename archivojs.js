23460f629845f9aa141874e78c019fee32b93c6d
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 00:44:39 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,7 +1,11 @@
 fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 .then(response => response.json())
 .then(data => { console.log(data.products)
-              document.getElementbyId("#myData").innerHTML = JSON.stringify(data.products[0].id)
+               var mainContainer = document.getElementById("myData");
+  for (let i = 0; i < 5; i++) {
+    var div = document.createElement("div");
+              div.innerHTML = JSON.stringify(data.products[0].id);
+     mainContainer.appendChild(div);
               })
 .catch(error => console.log(error))
 
