62bc1c513ecf7762475163f0914cf2923eb3b85e
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 22:30:19 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,13 +1,8 @@
-let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 
 .then(response => response.json)
 .then(data => console.log(data))
 .catch(error => console.log(error))
-
-
-
-console.log(promise) 
-console.log(typeof promise)
 
 
 function appendData(data) {
