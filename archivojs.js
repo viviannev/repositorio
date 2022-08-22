f50dd329b8386a14672f17b3540308bd059b0f1a
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 21:07:26 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -10,11 +10,14 @@ console.log(promise)
 console.log(typeof promise)
 
 
-function creardiv(){
-let div = document.createElement('p');
-    p.id = 'container';
-    p.innerHTML = 'Hi there!';
-}
+var mainContainer = document.getElementById("myData");
+ function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
+                mainContainer.appendChild(div);
+            }
   
 
   
