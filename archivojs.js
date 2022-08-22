77211978ed517510b4eef7a4bbace2b33426c753
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 01:33:12 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -7,7 +7,35 @@ fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
                  "Cost: " + data.products[0].cost + "<br />" +
                  "Currency: " + data.products[0].currency + "<br />" +
                  "SoldCount: " + data.products[0].soldCount + "<br />" +
-                 "Image: " + data.products[0].image               
+                 "Image: " + data.products[0].image + "<br />" +
+                  "Id: " + data.products[1].id + "<br />" +
+                 "Name: " + data.products[1].name + "<br />" +
+                 "Description: " + data.products[1].description + "<br />" +
+                 "Cost: " + data.products[1].cost + "<br />" +
+                 "Currency: " + data.products[1].currency + "<br />" +
+                 "SoldCount: " + data.products[1].soldCount + "<br />" +
+                 "Image: " + data.products[1].image + "<br />" +
+                  "Id: " + data.products[2].id + "<br />" +
+                 "Name: " + data.products[2].name + "<br />" +
+                 "Description: " + data.products[2].description + "<br />" +
+                 "Cost: " + data.products[2].cost + "<br />" +
+                 "Currency: " + data.products[2].currency + "<br />" +
+                 "SoldCount: " + data.products[2].soldCount + "<br />" +
+                 "Image: " + data.products[2].image + "<br />" +
+                  "Id: " + data.products[3].id + "<br />" +
+                 "Name: " + data.products[3].name + "<br />" +
+                 "Description: " + data.products[3].description + "<br />" +
+                 "Cost: " + data.products[3].cost + "<br />" +
+                 "Currency: " + data.products[3].currency + "<br />" +
+                 "SoldCount: " + data.products[3].soldCount + "<br />" +
+                 "Image: " + data.products[3].image + "<br />" +
+                  "Id: " + data.products[4].id + "<br />" +
+                 "Name: " + data.products[4].name + "<br />" +
+                 "Description: " + data.products[4].description + "<br />" +
+                 "Cost: " + data.products[4].cost + "<br />" +
+                 "Currency: " + data.products[4].currency + "<br />" +
+                 "SoldCount: " + data.products[4].soldCount + "<br />" +
+                 "Image: " + data.products[4].image + "<br />" +
               })
 .catch(error => console.log(error))
 
