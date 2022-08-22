9967421b32271f1e4e86f11eec239515f0e137c2
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 21:37:10 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -10,7 +10,7 @@ console.log(promise)
 console.log(typeof promise)
 
  function appendData(data) {
-            promise.forEach(let i = 0; i < data.length; i++); {
+            promise.forEach(let i = 0; i < data.length; i++) {
                 var div = document.createElement("div");    
                 document.getElementById("myData").innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
             }}
