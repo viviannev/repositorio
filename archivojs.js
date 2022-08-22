8bb544854fe933657d30e257b1efab21615fa25b
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 23:25:10 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,13 +1,34 @@
-let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+   fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+ .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+var mainContainer = document.getElementById("myData");
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (let i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
+ mainContainer.appendChild(div);
+  }
+}
 
-.then(response => response.json)
-.then(data => console.log(data))
-.catch(error => console.log(error))
+
+// let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
+
+//.then(response => response.json)
+//.then(data => console.log(data))
+//.catch(error => console.log(error))
 
 
 
-console.log(promise) 
-console.log(typeof promise)
+//console.log(promise) 
+//console.log(typeof promise)
 
-let listado = JSON.stringify(promise);
-console.log(listado.products);
+//let listado = JSON.stringify(promise);
+//console.log(listado.products); 
