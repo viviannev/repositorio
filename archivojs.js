969d75ca2fd41a71d3eebd860fd11cf2b07ca6c6
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 00:07:24 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,4 +1,4 @@
 fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => console.log(data.products))
 .catch(error => console.log(error))
