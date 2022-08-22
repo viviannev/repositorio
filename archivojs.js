79b5cef498e857377d7702a09a5b708a1ca01728
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 23:33:01 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -10,13 +10,13 @@ function appendData(data) {
     var div = document.createElement("div");
     div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
  mainContainer.appendChild(div);
-  }});
+  }}});
   
   .catch(function (err) {
     console.log(err);
   });
 
-}
+ 
 
 
 // let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
