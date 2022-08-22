5240a966d631fbe6bf2f404639f6ca0971505669
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-21 21:15:25 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,20 +1,18 @@
-fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+let promise = fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 
+.then(response => response.json)
+.then(data => console.log(data))
+.catch(error => console.log(error))
+
+
+
+console.log(promise) 
+console.log(typeof promise)
 
 var mainContainer = document.getElementById("myData");
  function appendData(data) {
             var mainContainer = document.getElementById("myData");
             for (var i = 0; i < data.length; i++) {
-                var div = document.createElement("div");
-                div.innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
-                mainContainer.appendChild(div);
+      
+                document.getElementbyId('myData').innerHTML = 'Id: ' + data[i].id + br + 'Name: ' + data[i].name + br + 'Description: ' + data[i].description + br + 'Cost: ' + data[i].cost + br + 'Currency: ' + data[i].currency + br + 'SoldCount: ' + data[i].soldCount + br + 'Image: ' + data[i].image;
             }}
