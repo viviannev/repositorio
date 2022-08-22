f9a9de849cbfe80dd4c634dfefc978002b8a982b
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 00:50:07 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,8 +1,7 @@
 fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 .then(response => response.json())
 .then(data => { console.log(data.products)
-               var maidat = document.getElementById("myData");
-              maidat.innerHTML = JSON.stringify(data.products[0].id);
+               document.getElementById("myData").innerText = "Your new text here" 
               })
 .catch(error => console.log(error))
 
