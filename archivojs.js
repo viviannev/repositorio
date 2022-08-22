fd63cd4a04880ad97c79dfae92beaf1eb2613fae
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 00:59:44 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,13 +1,7 @@
 fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 .then(response => response.json())
 .then(data => { console.log(data.products)
-               document.querySelector("#myData").innerText = "Id: " + data.products[0].id + <br> 
-               "Name: " + data.products[0].name + <br>
-               "Description: " + data.products[0].description + <br>
-               "Cost: " + data.products[0].cost + <br>
-               "Currency: " + data.products[0].currency + <br>
-               "SoldCount: " + data.products[0].soldCount + <br>
-               "Image: " + data.products[0].image + <br>
+               document.querySelector("#myData").innerText = "Id: " + data.products[0].id + "Name: " + data.products[0].name + "Description: " + data.products[0].description + "Cost: " + data.products[0].cost + "Currency: " + data.products[0].currency + "SoldCount: " + data.products[0].soldCount + "Image: " + data.products[0].image
                
               })
 .catch(error => console.log(error))
