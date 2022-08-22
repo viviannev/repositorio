0e75f5c3d0c39ee8c2edb7bd3fc1dffbc85713ca
viviannev <110606808+viviannev@users.noreply.github.com>
2022-08-22 01:35:59 -0300

    Update archivojs.js

--- archivojs.js
+++ archivojs.js
@@ -1,34 +1,34 @@
 fetch ('https://japceibal.github.io/emercado-api/cats_products/101.json')
 .then(response => response.json())
 .then(data => { console.log(data.products)
-               document.querySelector("#myData").innerHTML = "Id: " + data.products[0].id + "<br />" +
+               document.querySelector("#myData").innerHTML = "<br />" + "Id: " + data.products[0].id + "<br />" +
                  "Name: " + data.products[0].name + "<br />" +
                  "Description: " + data.products[0].description + "<br />" +
                  "Cost: " + data.products[0].cost + "<br />" +
                  "Currency: " + data.products[0].currency + "<br />" +
                  "SoldCount: " + data.products[0].soldCount + "<br />" +
-                 "Image: " + data.products[0].image + "<br />" +
+                 "Image: " + data.products[0].image + "<br />" + "<br />" +
                   "Id: " + data.products[1].id + "<br />" +
                  "Name: " + data.products[1].name + "<br />" +
                  "Description: " + data.products[1].description + "<br />" +
                  "Cost: " + data.products[1].cost + "<br />" +
                  "Currency: " + data.products[1].currency + "<br />" +
                  "SoldCount: " + data.products[1].soldCount + "<br />" +
-                 "Image: " + data.products[1].image + "<br />" +
+                 "Image: " + data.products[1].image + "<br />" + "<br />" +
                   "Id: " + data.products[2].id + "<br />" +
                  "Name: " + data.products[2].name + "<br />" +
                  "Description: " + data.products[2].description + "<br />" +
                  "Cost: " + data.products[2].cost + "<br />" +
                  "Currency: " + data.products[2].currency + "<br />" +
                  "SoldCount: " + data.products[2].soldCount + "<br />" +
-                 "Image: " + data.products[2].image + "<br />" +
+                 "Image: " + data.products[2].image + "<br />" + "<br />" +
                   "Id: " + data.products[3].id + "<br />" +
                  "Name: " + data.products[3].name + "<br />" +
                  "Description: " + data.products[3].description + "<br />" +
                  "Cost: " + data.products[3].cost + "<br />" +
                  "Currency: " + data.products[3].currency + "<br />" +
                  "SoldCount: " + data.products[3].soldCount + "<br />" +
-                 "Image: " + data.products[3].image + "<br />" +
+                 "Image: " + data.products[3].image + "<br />" + "<br />" +
                   "Id: " + data.products[4].id + "<br />" +
                  "Name: " + data.products[4].name + "<br />" +
                  "Description: " + data.products[4].description + "<br />" +
